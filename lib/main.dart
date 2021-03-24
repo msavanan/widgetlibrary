f28e59d1dd6ff55138e50942129a21cf1100addd
msavanan <saravanan.rnd@gmail.com>
2021-03-24 15:44:59 +0530

    added gestureDectecter to show a container widget over scaffold

--- lib/main.dart
+++ lib/main.dart
@@ -45,6 +45,8 @@ class _MyHomePageState extends State<MyHomePage> {
 
   Location newLocation = Location();
 
+  bool swipeDirection = false;
+
   List<double> loc = [0, 0];
 
   @override
@@ -55,179 +57,223 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: SingleChildScrollView(
-        child: Center(
-          child: Padding(
-            padding: EdgeInsets.all(10),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                SizedBox(height: 10),
-                Text('Animation in a button'),
-                SizedBox(
-                  height: 60,
-                  width: 60,
-                  child: TextButton(
-                    style: ButtonStyle(
-                        tapTargetSize: MaterialTapTargetSize.padded),
-                    child: Image(image: AssetImage("assets/gift-box-2.gif")),
-                    onPressed: () {},
-                  ),
-                ),
-                SizedBox(height: 10),
-                Padding(
-                    padding: EdgeInsets.only(bottom: 2, left: 10),
-                    child: Text("Horizontal Scroll")),
-                SizedBox(height: 10),
-                Container(
-                  height: 60,
-                  child: ListView(
-                    scrollDirection: Axis.horizontal,
-                    children: [
-                      SizedBox(width: 10),
-                      ElevatedButton(
-                          child: Icon(Icons.location_on), onPressed: () {}),
-                      SizedBox(width: 10),
-                      ElevatedButton(
-                          child: Icon(Icons.email), onPressed: () {}),
-                      SizedBox(width: 10),
-                      ElevatedButton(child: Icon(Icons.home), onPressed: () {}),
-                      SizedBox(width: 10),
-                      ElevatedButton(
-                          child: Icon(Icons.access_alarm_outlined),
-                          onPressed: () {}),
-                      SizedBox(width: 10),
-                      ElevatedButton(
-                          child: Icon(Icons.accessibility), onPressed: () {}),
-                      SizedBox(width: 10),
-                      ElevatedButton(
-                          child: Icon(Icons.ac_unit), onPressed: () {}),
-                    ],
-                  ),
-                ),
-                SizedBox(height: 10),
-                Text("Grouped Buttons"),
-                SizedBox(height: 10),
-                Container(
-                  height: 140,
-                  padding: EdgeInsets.only(top: 30, bottom: 20, left: 20),
-                  color: Color(0XFFeeeeee),
+    return GestureDetector(
+      onPanUpdate: (details) {
+        print(details);
+        if (details.delta.dx > 0) {
+          print("swiped in the right direction");
+          setState(() {
+            swipeDirection = true;
+          });
+
+          // swiping in right direction
+        } else if (details.delta.dx < 0) {
+          print("swiped in the left direction");
+          setState(() {
+            swipeDirection = false;
+          });
+        }
+      },
+      onTap: () {
+        print("Tapped");
+        setState(() {
+          swipeDirection = false;
+        });
+      },
+      child: Stack(
+        children: [
+          Scaffold(
+            appBar: AppBar(
+              title: Text(widget.title),
+            ),
+            body: SingleChildScrollView(
+              child: Center(
+                child: Padding(
+                  padding: EdgeInsets.all(10),
                   child: Column(
                     mainAxisAlignment: MainAxisAlignment.start,
-                    crossAxisAlignment: CrossAxisAlignment.stretch,
-                    children: [
-                      Text(
-                        'Price Range',
-                        style: TextStyle(
-                          fontSize: 24,
-                          fontWeight: FontWeight.w500,
-                          color: Colors.black,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      SizedBox(height: 10),
+                      Text('Animation in a button'),
+                      SizedBox(
+                        height: 60,
+                        width: 60,
+                        child: TextButton(
+                          style: ButtonStyle(
+                              tapTargetSize: MaterialTapTargetSize.padded),
+                          child:
+                              Image(image: AssetImage("assets/gift-box-2.gif")),
+                          onPressed: () {},
+                        ),
+                      ),
+                      SizedBox(height: 10),
+                      Padding(
+                          padding: EdgeInsets.only(bottom: 2, left: 10),
+                          child: Text("Horizontal Scroll")),
+                      SizedBox(height: 10),
+                      Container(
+                        height: 60,
+                        child: ListView(
+                          scrollDirection: Axis.horizontal,
+                          children: [
+                            SizedBox(width: 10),
+                            ElevatedButton(
+                                child: Icon(Icons.location_on),
+                                onPressed: () {}),
+                            SizedBox(width: 10),
+                            ElevatedButton(
+                                child: Icon(Icons.email), onPressed: () {}),
+                            SizedBox(width: 10),
+                            ElevatedButton(
+                                child: Icon(Icons.home), onPressed: () {}),
+                            SizedBox(width: 10),
+                            ElevatedButton(
+                                child: Icon(Icons.access_alarm_outlined),
+                                onPressed: () {}),
+                            SizedBox(width: 10),
+                            ElevatedButton(
+                                child: Icon(Icons.accessibility),
+                                onPressed: () {}),
+                            SizedBox(width: 10),
+                            ElevatedButton(
+                                child: Icon(Icons.ac_unit), onPressed: () {}),
+                          ],
+                        ),
+                      ),
+                      SizedBox(height: 10),
+                      Text("Grouped Buttons"),
+                      SizedBox(height: 10),
+                      Container(
+                        height: 140,
+                        padding: EdgeInsets.only(top: 30, bottom: 20, left: 20),
+                        color: Color(0XFFeeeeee),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          crossAxisAlignment: CrossAxisAlignment.stretch,
+                          children: [
+                            Text(
+                              'Price Range',
+                              style: TextStyle(
+                                fontSize: 24,
+                                fontWeight: FontWeight.w500,
+                                color: Colors.black,
+                              ),
+                            ),
+                            SizedBox(
+                              height: 10,
+                            ),
+                            GroupedButtons(),
+                          ],
                         ),
                       ),
                       SizedBox(
                         height: 10,
                       ),
-                      GroupedButtons(),
+                      Text('Drawer in a Stack Widget'),
+                      ElevatedButton(
+                          onPressed: () {
+                            Navigator.of(context).push(MaterialPageRoute(
+                                builder: (context) => DrawerStackWidget()));
+                          },
+                          child: Text("Drawer Widget in a Stack")),
+                      Text('Icon Animation'),
+                      AnimateButton(duration: Duration(milliseconds: 450)),
+                      ElevatedButton(
+                          onPressed: () async {
+                            final cameras = await availableCameras();
+                            final firstCamera = cameras.first;
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => TakePictureScreen(
+                                  camera: firstCamera,
+                                ),
+                              ),
+                            );
+                          },
+                          child: Text("Camera - Click me")),
+                      Text('Sliding ListView'),
+                      ElevatedButton(
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => SlidingListView(),
+                            ),
+                          );
+                        },
+                        child: Text('Sliding ListView'),
+                      ),
+                      ElevatedButton(
+                          onPressed: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => AccountCards()),
+                            );
+                          },
+                          child: Text('ICICI Card')),
+                      Text('GPS position'),
+                      Container(
+                        width: MediaQuery.of(context).size.width,
+                        height: MediaQuery.of(context).size.height * 0.05,
+                        child: TextButton(
+                            style: TextButton.styleFrom(
+                                backgroundColor: Colors.amberAccent),
+                            onPressed: () async {
+                              try {
+                                if (newLocation.longitude == null ||
+                                    newLocation.latitude == null) {
+                                  await newLocation.getCurrentLocation();
+                                }
+                                setState(() {
+                                  loc[0] = newLocation.longitude;
+                                  loc[1] = newLocation.latitude;
+                                });
+                              } catch (e) {
+                                print(
+                                    '${newLocation.latitude}, ${newLocation.longitude}');
+                              }
+                              print(
+                                  '${newLocation.latitude}, ${newLocation.longitude}');
+                            },
+                            child: Text('${loc[0] ?? 0}, ${loc[1] ?? 0}')),
+                      ),
+                      ElevatedButton(
+                          onPressed: () {
+                            Navigator.of(context)
+                                .push(MaterialPageRoute(builder: (context) {
+                              return CarouselDemo();
+                            }));
+                          },
+                          child: Text("Carousel Demo")),
+                      ElevatedButton(
+                          onPressed: () {
+                            Navigator.of(context)
+                                .push(MaterialPageRoute(builder: (context) {
+                              return DatePicker();
+                            }));
+                          },
+                          child: Text('Date Picker')),
                     ],
                   ),
                 ),
-                SizedBox(
-                  height: 10,
-                ),
-                Text('Drawer in a Stack Widget'),
-                ElevatedButton(
-                    onPressed: () {
-                      Navigator.of(context).push(MaterialPageRoute(
-                          builder: (context) => DrawerStackWidget()));
-                    },
-                    child: Text("Drawer Widget in a Stack")),
-                Text('Icon Animation'),
-                AnimateButton(duration: Duration(milliseconds: 450)),
-                ElevatedButton(
-                    onPressed: () async {
-                      final cameras = await availableCameras();
-                      final firstCamera = cameras.first;
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => TakePictureScreen(
-                            camera: firstCamera,
-                          ),
-                        ),
-                      );
-                    },
-                    child: Text("Camera - Click me")),
-                Text('Sliding ListView'),
-                ElevatedButton(
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => SlidingListView(),
-                      ),
-                    );
-                  },
-                  child: Text('Sliding ListView'),
-                ),
-                ElevatedButton(
-                    onPressed: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (context) => AccountCards()),
-                      );
-                    },
-                    child: Text('ICICI Card')),
-                Text('GPS position'),
-                Container(
-                  width: MediaQuery.of(context).size.width,
-                  height: MediaQuery.of(context).size.height * 0.05,
-                  child: TextButton(
-                      style: TextButton.styleFrom(
-                          backgroundColor: Colors.amberAccent),
-                      onPressed: () async {
-                        try {
-                          if (newLocation.longitude == null ||
-                              newLocation.latitude == null) {
-                            await newLocation.getCurrentLocation();
-                          }
-                          setState(() {
-                            loc[0] = newLocation.longitude;
-                            loc[1] = newLocation.latitude;
-                          });
-                        } catch (e) {
-                          print(
-                              '${newLocation.latitude}, ${newLocation.longitude}');
-                        }
-                        print(
-                            '${newLocation.latitude}, ${newLocation.longitude}');
-                      },
-                      child: Text('${loc[0] ?? 0}, ${loc[1] ?? 0}')),
-                ),
-                ElevatedButton(
-                    onPressed: () {
-                      Navigator.of(context)
-                          .push(MaterialPageRoute(builder: (context) {
-                        return CarouselDemo();
-                      }));
-                    },
-                    child: Text("Carousel Demo")),
-                ElevatedButton(
-                    onPressed: () {
-                      Navigator.of(context)
-                          .push(MaterialPageRoute(builder: (context) {
-                        return DatePicker();
-                      }));
-                    },
-                    child: Text('Date Picker'))
-              ],
+              ),
             ),
           ),
-        ),
+          swipeDirection
+              ? SafeArea(
+                  child: Container(
+                    width: MediaQuery.of(context).size.width * 0.75,
+                    height: MediaQuery.of(context).size.height,
+                    color: Colors.white,
+                  ),
+                )
+              : Container(
+                  //color: Colors.amber,
+                  ),
+        ],
       ),
     );
   }
